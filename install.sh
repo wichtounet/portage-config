05f1cf4babeb6d59688c947d66a95f59d147476e
Baptiste Wicht <baptiste.wicht@gmail.com>
2013-06-19 09:30:23 +0200

    Create install script

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ln -s make.conf /etc/portage/make.conf
+ln -s package.use /etc/portage/package.use
+ln -s package.mask /etc/portage/package.mask
+ln -s package.license /etc/portage/package.license
+ln -s package.accept_keywords /etc/portage/package.accept_keywords
