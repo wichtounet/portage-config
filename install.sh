c70d387c1b5bfd308fe60d4fdbd40bd8e663f2e5
Baptiste Wicht <baptiste.wicht@gmail.com>
2013-06-27 13:26:17 +0200

    Update installation script for world

--- install.sh
+++ install.sh
@@ -5,9 +5,11 @@ sudo mv -f /etc/portage/package.use /etc/portage/package.use.bak
 sudo mv -f /etc/portage/package.mask /etc/portage/package.mask.bak
 sudo mv -f /etc/portage/package.license /etc/portage/package.license.bak
 sudo mv -f /etc/portage/package.accept_keywords /etc/portage/package.accept_keywords.bak
+sudo mv -f /var/lib/portage/world /var/lib/portage/world.bak
 
 sudo ln -s $(pwd)/make.conf /etc/portage/make.conf
 sudo ln -s $(pwd)/package.use /etc/portage/package.use
 sudo ln -s $(pwd)/package.mask /etc/portage/package.mask
 sudo ln -s $(pwd)/package.license /etc/portage/package.license
 sudo ln -s $(pwd)/package.accept_keywords /etc/portage/package.accept_keywords
+sudo ln -s $(pwd)/world /var/lib/portage/world
